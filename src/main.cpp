caa730509ebd02eeeb4986fcd9aed7764217ca1b
Danyil Somin <danyil.somin@gmail.com>
2023-09-15 01:12:11 +0300

    Add example opening window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,40 @@
-#include <iostream>
 #include <SDL3/SDL.h>
+#include <iostream>
 
 int main() {
-    std::cout << "Hello World!\n";
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cout << "Failed to initialize the SDL library\n";
+    std::cout << "SDL Error: " << SDL_GetError() << "\n";
+    return -1;
+  }
+
+  SDL_Window *window = SDL_CreateWindow("SDL Window", 680, 480, 0);
+
+  if (!window) {
+    std::cout << "Failed to create window\n";
+    std::cout << "SDL Error: " << SDL_GetError() << "\n";
+    return -1;
+  }
+
+  SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+  if (!window_surface) {
+    std::cout << "Failed to get the surface from the window\n";
+    std::cout << "SDL Error: " << SDL_GetError() << "\n";
+    return -1;
+  }
+
+  bool keep_window_open = true;
+  while (keep_window_open) {
+    SDL_Event e;
+    while (SDL_PollEvent(&e) > 0) {
+      switch (e.type) {
+      case SDL_EVENT_QUIT:
+        keep_window_open = false;
+        break;
+      }
+
+      SDL_UpdateWindowSurface(window);
+    }
+  }
 }
